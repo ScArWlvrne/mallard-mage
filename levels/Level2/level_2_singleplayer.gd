4d6f1a60e7ccbb8365737739bc810763632b8b87
Hoang Le <leh7@oregonstate.edu>
2025-11-16 09:06:59 -0800

    goalpost 2nd level

---
+++ levels/Level2/level_2_singleplayer.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mana_component = $Level2/Player/ManaComponent
+	var mana_bar = $UILayer/ManaBar
+
+	# Connect the signal
+	mana_component.connect("mana_changed", Callable(self, "_on_mana_changed"))
+
+	# Update the bar immediately
+	mana_bar.max_value = mana_component.max_mana
+	mana_bar.value = mana_component.current_mana
+
+
+func _on_mana_changed(current: int, max: int) -> void:
+	$UILayer/ManaBar.max_value = max
+	$UILayer/ManaBar.value = current
