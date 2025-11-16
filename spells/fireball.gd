86541328d9dffb6d85e07ad7c0fc5969b72abeda
Kyran <kyran@dyn-10-108-9-137.wless.uoregon.edu>
2025-11-15 22:39:12 -0800

    fireballs from the butt

---
+++ spells/fireball.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var speed: float = 300.0  # pixels per second
+var velocity: Vector2 = Vector2.ZERO
+
+
+func shoot(start_position: Vector2, angle_radians: float) -> void:
+	global_position = start_position       # Set where the fireball spawns
+	rotation = angle_radians               # Visually rotate it
+	velocity = Vector2.RIGHT.rotated(angle_radians) * speed  # Move in that direction
+	
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
